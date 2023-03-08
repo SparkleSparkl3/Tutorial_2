1a657aaac71b6377255b8a7962460241dcd20abf
SparkleSparkl3 <122640913+SparkleSparkl3@users.noreply.github.com>
2023-03-07 19:45:53 -0500

    Fourth vid done done

--- Tut_2/Assets/Scripts/PlayerScript.cs
+++ Tut_2/Assets/Scripts/PlayerScript.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class PlayerScript : MonoBehaviour
 {
@@ -8,10 +11,15 @@ public class PlayerScript : MonoBehaviour
 
     public float speed;
 
+    public TextMeshProUGUI score;
+
+    private int scoreValue = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
+        score.text = scoreValue.ToString();
     }
 
     // Update is called once per frame
@@ -20,5 +28,27 @@ public class PlayerScript : MonoBehaviour
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if (collision.collider.tag == "Coin")
+        {
+            scoreValue += 1;
+            score.text = scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 2), ForceMode2D.Impulse); 
+            }
+        }
     }
 }
